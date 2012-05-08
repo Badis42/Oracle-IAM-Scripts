997ab6f6aa7713a95246184af992026dc1b745c0
Chris Johnson <chris@three12.com>
2012-05-08 13:12:06 -0400

    RCU requires libaio.i686 on OL6 for some product schemas

--- installscripts/12_rculibs.sh
+++ installscripts/12_rculibs.sh
@@ -8,7 +8,9 @@ if [ "$OS_VER" == "5" ]; then
 
 elif [ "$OS_VER" == "6" ]; then
     yum -y install libgcc.i686  compat-libstdc++-33.i686
+
+    # these could all be on the same line
+    yum -y install libaio.i686
     yum -y install libXext.i686
     yum -y install libXtst.i686
 fi
-
