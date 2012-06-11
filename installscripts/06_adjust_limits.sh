e1f4dd7053ce5b57fc31829a527005828b0dd559
Chris Johnson <chris@three12.com>
2012-06-11 18:29:50 -0400

    Fixes to limits if the database is running as a user other than "oracle"

--- installscripts/06_adjust_limits.sh
+++ installscripts/06_adjust_limits.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+# load settings
+. settings.sh
 
 # need to determine whether we're on OEL5 or OEL6
 . osver.sh
 
 if [ "$OS_VER" == "5" ]; then
-    echo The oracle-validated package takes care of all of this for you
+    echo The oracle-validated package takes care of MOST of this for you
 
 elif [ $"$OS_VER" == "6" ]; then
-    echo The oracle-rdbms-server-11gR2-preinstall package takes care of all of this for you
+    echo The oracle-rdbms-server-11gR2-preinstall package takes care of MOST of this for you
 fi
 
+
+if [ "$ORA_USER" != "oracle" ]; then
+    echo "Database will not be installed as oracle."
+    echo "Limits need to be adjusted for $ORA_USER."
+
+    cat >> /etc/security/limits.conf <<EOF
+$ORA_USER           soft    nproc   2047
+$ORA_USER           hard    nproc   16384
+$ORA_USER           soft    nofile  1024
+$ORA_USER           hard    nofile  65536
+EOF
+
+fi
