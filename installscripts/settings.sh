8265b1d9445f8699ab5de95ec0cf09e6fdcfdf9d
Chris Johnson <chris@three12.com>
2012-06-11 18:28:54 -0400

    Settings like usernames and passwords moved to a single location

---
+++ installscripts/settings.sh
@@ -0,0 +1,16 @@
+# this script is intended to be used by the other ones.
+# don't run it directly!
+
+# what user?
+ORA_USER=fmwuser
+ORA_PASS=ABcd1234
+
+USER_HOME=/scratch/$ORA_USER
+
+# where do you want the database
+ORADB_LOCATION=$USER_HOME/database
+
+# and the FMW location
+FMW_LOCATION=$USER_HOME/middleware
+
+VNC_PASS=ABcd1234
