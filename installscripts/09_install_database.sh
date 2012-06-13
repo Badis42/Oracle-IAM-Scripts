0bd64ba3f17bb7690b9b78e59a47e2d826dd3ee3
Chris Johnson <chris@three12.com>
2012-06-12 21:02:07 -0400

    First cut at doing an automated install of the database.

--- installscripts/09_install_database.sh
+++ installscripts/09_install_database.sh
@@ -1,6 +1,77 @@
 #!/bin/bash
 
-echo "STOP!"
-echo "You are supposed to unzip the database installer and install it"
-echo "manually now"
+. `dirname $0`/settings.sh
 
+if [ "$USER" == "" ]; then
+    echo Could not determine user identity.
+    exit 1
+elif [ "$USER" == "root" ]; then
+    echo "STOP!"
+    echo "You are supposed to unzip the database installer and install it"
+    echo "either by hand or with this script"
+elif [ "$USER" != "$ORA_USER" ]; then
+    echo "STOP!"
+    echo "You should be running this as $ORA_USER"
+fi
+
+
+TEMPFILE=/tmp/$$.rsp
+
+cat > $TEMPFILE <<EOF
+oracle.install.responseFileVersion=/oracle/install/rspfmt_dbinstall_response_schema_v11_2_0
+oracle.install.option=INSTALL_DB_AND_CONFIG
+ORACLE_HOSTNAME=localhost
+UNIX_GROUP_NAME=oinstall
+INVENTORY_LOCATION=$USER_HOME/oraInventory
+SELECTED_LANGUAGES=en
+ORACLE_HOME=$ORADB_LOCATION/product/11.2.0/dbhome_1
+ORACLE_BASE=$ORADB_LOCATION
+oracle.install.db.InstallEdition=EE
+oracle.install.db.EEOptionsSelection=false
+oracle.install.db.optionalComponents=
+oracle.install.db.DBA_GROUP=dba
+oracle.install.db.OPER_GROUP=dba
+oracle.install.db.CLUSTER_NODES=
+oracle.install.db.isRACOneInstall=false
+oracle.install.db.racOneServiceName=
+oracle.install.db.config.starterdb.type=GENERAL_PURPOSE
+oracle.install.db.config.starterdb.globalDBName=$DB_SID
+oracle.install.db.config.starterdb.SID=$DB_SID
+oracle.install.db.config.starterdb.characterSet=AL32UTF8
+oracle.install.db.config.starterdb.memoryOption=true
+oracle.install.db.config.starterdb.memoryLimit=2285
+oracle.install.db.config.starterdb.installExampleSchemas=true
+oracle.install.db.config.starterdb.enableSecuritySettings=true
+oracle.install.db.config.starterdb.password.ALL=$ORA_PASS
+oracle.install.db.config.starterdb.password.SYS=$ORA_PASS
+oracle.install.db.config.starterdb.password.SYSTEM=$ORA_PASS
+oracle.install.db.config.starterdb.password.SYSMAN=$ORA_PASS
+oracle.install.db.config.starterdb.password.DBSNMP=$ORA_PASS
+oracle.install.db.config.starterdb.control=DB_CONTROL
+oracle.install.db.config.starterdb.gridcontrol.gridControlServiceURL=
+oracle.install.db.config.starterdb.automatedBackup.enable=false
+oracle.install.db.config.starterdb.automatedBackup.osuid=
+oracle.install.db.config.starterdb.automatedBackup.ospwd=
+oracle.install.db.config.starterdb.storageType=FILE_SYSTEM_STORAGE
+oracle.install.db.config.starterdb.fileSystemStorage.dataLocation=$ORADB_LOCATION/oradata
+oracle.install.db.config.starterdb.fileSystemStorage.recoveryLocation=
+oracle.install.db.config.asm.diskGroup=
+oracle.install.db.config.asm.ASMSNMPPassword=
+MYORACLESUPPORT_USERNAME=
+MYORACLESUPPORT_PASSWORD=
+SECURITY_UPDATES_VIA_MYORACLESUPPORT=false
+DECLINE_SECURITY_UPDATES=true
+PROXY_HOST=
+PROXY_PORT=
+PROXY_USER=
+PROXY_PWD=
+PROXY_REALM=
+COLLECTOR_SUPPORTHUB_URL=
+oracle.installer.autoupdates.option=SKIP_UPDATES
+oracle.installer.autoupdates.downloadUpdatesLoc=
+AUTOUPDATES_MYORACLESUPPORT_USERNAME=
+AUTOUPDATES_MYORACLESUPPORT_PASSWORD=
+EOF
+
+#~cmjohnso/installers/ss3-refresh/DB/Disk1/runInstaller -silent -responseFile $TEMPFILE
+$INSTALLER_LOCATION/$VERSION_TO_INSTALL/DB/Disk1/runInstaller -silent -responseFile $TEMPFILE
--- installscripts/settings.sh
+++ installscripts/settings.sh
@@ -14,3 +14,26 @@ ORADB_LOCATION=$USER_HOME/database
 FMW_LOCATION=$USER_HOME/middleware
 
 VNC_PASS=ABcd1234
+
+# Temporary workspace
+# "small" files (like generated rsp files and the like) go in /tmp
+# larger items (like unzipped installers) go in this directory
+WORKSPACE=~/installbits
+
+
+# These settings are related to stuff on my environment
+INSTALLER_LOCATION=~cmjohnso/installers
+VERSION_TO_INSTALL=ss3-refresh
+
+DB_HOST=localhost
+DB_PORT=1521
+DB_SID=orcl
+
+# this is for the RCU:
+CONNECT_STRING=$DB_HOST:$DB_PORT:$DB_SID
+SCHEMA_PREFIX=DEV
+
+# WebLogic
+WLSVERSION=10.3.6
+
+# IAM bits
