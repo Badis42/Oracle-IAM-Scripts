1edbde36caec810509a59b9af520227333097618
Chris Johnson <chris@three12.com>
2012-06-12 21:00:15 -0400

    Tweak lines calling osver.sh and settings.sh so that you can run the scripts from anywhere

--- installscripts/13_removejava.sh
+++ installscripts/13_removejava.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-. osver.sh
+. `dirname $0`/osver.sh
 
 if [ "$OS_VER" == "5" ]; then
     rpm -e java-1.4.2-gcj-compat-1.4.2.0-40jpp.115 gjdoc-0.7.7-12.el5.x86_64 antlr-2.7.6-4jpp.2.x86_64
